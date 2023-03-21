8a52905497d70e148c1a6940c158b961a3e9f85d
Poundpitak Amornlerdlittichai <Damon1FX@gmail.com>
2023-03-21 14:58:53 +0700

    Update index.ts

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,5 +1,14 @@
 import express from "express";
+import validateToken from "../middleware/validateToken.middleware";
+import authController from "../controllers/auth/authController";
 
 const router = express.Router();
+const authRoutes = express.Router();
+
+authRoutes.post("/login", authController.Login);
+authRoutes.post("/register", authController.Register);
+authRoutes.get("/logout", validateToken, authController.Logout);
+
+router.use("/auth", authRoutes);
 
 export default router;
